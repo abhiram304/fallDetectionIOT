752e1e91fd8800e9f58731736d02917d14a4d718
Abhi <you@example.com>
2017-10-29 15:08:23 -0700

    with dashboard

--- routes/user.js
+++ routes/user.js
@@ -11,3 +11,10 @@ exports.receiveData = function(req, res){
 	var data= req.param("value");  
 	res.send("data: "+data+" is stored to the server." );
 	};
+	
+	
+	
+	exports.getDashboard = function(req, res){
+		//var data= req.param("value");  
+		res.render('dashboard');
+		};
